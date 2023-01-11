da7db6076272f3c245e4d23b894aed1799488c66
jmnaze123 <jmnaze123@gmail.com>
2023-01-10 23:56:32 -0500

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -19,8 +19,7 @@ function importComponent(component) {
     })
     document.body.appendChild(component);
 }
-// importComponent("trailer");
-importComponent("clock")
+importComponent("trailer");
 
 function createApp() {
 
